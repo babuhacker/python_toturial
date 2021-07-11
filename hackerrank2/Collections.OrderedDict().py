450349209c54239d99fff8c54bfb24cbe2ba2727
babuhacker <kashyapaditya2077@gmail.com>
2021-07-11 15:25:12 +0530

    HackerRank Starting

---
+++ hackerrank2/Collections.OrderedDict().py
@@ -0,0 +1,13 @@
+from collections import OrderedDict
+
+n = int(input())
+d = OrderedDict()
+for i in range(n):
+    item, space, number = input().rpartition(" ")
+    number = int(number)
+    if item in d.keys():
+        d[item] += number
+    else:
+        d[item] = number
+for i in d.keys():
+    print(i, d[i])
